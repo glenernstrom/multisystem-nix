a36c9c12d5a5e5c011d718b6ed2c6ffcee128ee4
Glen Ernstrom <otterbrain@protonmail.com>
2026-09-17 15:17:39 -0400

    Configure minimal encrypted Puma system

--- hosts/puma/default.nix
+++ hosts/puma/default.nix
@@ -8,5 +8,9 @@
 
   networking.hostName = "puma";
 
+  # Encrypted swap
+  boot.initrd.luks.devices."luks-4941926a-c221-4242-be63-202236f9e0b3".device =
+  "/dev/disk/by-uuid/4941926a-c221-4242-be63-202236f9e0b3";
+
   system.stateVersion = "26.05";
 }
--- modules/core/base.nix
+++ modules/core/base.nix
@@ -14,6 +14,17 @@
 
   i18n.defaultLocale = "en_US.UTF-8";
 
+  users.users.glen = {
+  isNormalUser = true;
+  description = "Glen Ernstrom";
+  extraGroups = [
+    "networkmanager"
+    "wheel"
+    ];
+  };
+
+  nixpkgs.config.allowUnfree = true;
+
   networking.networkmanager.enable = true;
 
   environment.systemPackages = with pkgs; [
