c836eb0c97fac6028680a8f25224c65fd2c387f9
Glen Ernstrom <otterbrain@protonmail.com>
2026-09-20 15:15:13 -0400

    Add workstation, development, gaming, and hardware modules

--- modules/roles/workstation.nix
+++ modules/roles/workstation.nix
@@ -1,7 +1,36 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
+  environment.systemPackages = with pkgs; [
+    rsync
+  ];
+
   services.flatpak.packages = [
     "org.libreoffice.LibreOffice"
+    "com.github.jeromerobert.pdfarranger"
+    "be.alexandervanhee.gradia"
+    "org.gimp.GIMP"
+    "org.inkscape.Inkscape"
+    "io.github.seadve.Kooha"
+    "com.obsproject.Studio"
+    "org.shotcut.Shotcut"
+    "com.github.xournalpp.xournalpp"
+    "org.gnome.DejaDup"
+    "com.pojtinger.felicitas.Sessions"
+    "com.rafaelmardojai.Blanket"
+    "net.cozic.joplin_desktop"
+    "page.wisha.platen"
+    "com.github.ahrm.sioyek"
+    "app.shizumu.Shizumu"
+    "org.gnome.Decibels"
+    "com.github.johnfactotum.Foliate"
+    "com.github.geigi.cozy"
+    "io.gitlab.news_flash.NewsFlash"
+    "de.haeckerfelix.Shortwave"
+    "com.obsproject.Studio"
+    "io.github.screwys.Rufin"
+    "dev.qwery.AddWater"
+    "io.gitlab.adhami3310.Impression"
+    "io.github.smolblackcat.Progress"
   ];
 }
