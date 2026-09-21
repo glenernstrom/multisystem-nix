1fe6a02def94f669a6e41fbdab55d84cc5cc9d2b
Glen Ernstrom <otterbrain@protonmail.com>
2026-09-21 10:44:05 -0400

    Flatpaks for puma's workstation

--- modules/roles/workstation.nix
+++ modules/roles/workstation.nix
@@ -3,5 +3,23 @@
 {
   services.flatpak.packages = [
     "org.libreoffice.LibreOffice"
+    "com.github.jeromerobert.pdfarranger"
+    "com.github.muriloventuroso.pdftricks"
+    "com.pojtinger.felicitas.Sessions"
+    "net.cozic.joplin_desktop"
+    "be.alexandervanhee.gradia"
+    "com.github.xournalpp.xournalpp"
+    "org.gimp.GIMP"
+    "org.inkscape.Inkscape"
+    "io.github.smolblackcat.Progress"
+    "re.sonny.Eloquent"
+    "com.trougnouf.Cfait"
+    "page.wisha.platen"
+    "org.speedynote.SpeedyNote"
+    "app.shizumu.Shizumu"
+    "org.jabref.jabref"
+    "page.codeberg.censor.Censor"
+    "me.dumke.Reinschrift"
+    "com.github.ahrm.sioyek"
   ];
 }
